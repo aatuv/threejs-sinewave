557cec508998d4c0de6c705d58bf7d75e5650304
Aatu Väisänen <aatu.visnen3@gmail.com>
2021-12-27 17:50:24 +0200

    changes

--- main.js
+++ main.js
@@ -2,7 +2,10 @@
 
 import * as THREE from "https://cdn.skypack.dev/three@0.124.0";
 
+const background = "#2c2a38";
+
 const scene = new THREE.Scene();
+scene.fog = new THREE.Fog(background, 1, 300000);
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -15,9 +18,10 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(background, 1);
 document.body.appendChild(renderer.domElement);
 
-const planeGeometry = new THREE.PlaneGeometry(10, 6, 100, 60);
+const planeGeometry = new THREE.PlaneGeometry(10, 3, 100, 30);
 const planeMaterial = new THREE.MeshBasicMaterial({
   color: 0x3ac3d3,
   opacity: 0,
@@ -25,22 +29,27 @@ const planeMaterial = new THREE.MeshBasicMaterial({
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
-let spheres = [];
+let circles = [];
 for (let i = 0; i < plane.geometry.vertices.length; i++) {
   const v = plane.geometry.vertices[i];
-  const sphereGeometry = new THREE.SphereGeometry(0.0125, 10, 10);
-  const sphereMaterial = new THREE.MeshBasicMaterial({
+  const circleGeometry = new THREE.CircleGeometry(0.01, 12);
+  const circleMaterial = new THREE.MeshBasicMaterial({
     color: 0x3ac3d3,
     opacity: 1,
   });
-  spheres[i] = new THREE.Mesh(sphereGeometry, sphereMaterial);
-  spheres[i].position.set(v.x, v.y, v.z);
-  scene.add(spheres[i]);
+  circles[i] = new THREE.Mesh(circleGeometry, circleMaterial);
+  circles[i].position.set(v.x, v.y, v.z);
+  circles[i].quaternion.copy(camera.quaternion);
+  scene.add(circles[i]);
 }
 
 camera.rotation.x = (95 * Math.PI) / 180;
 
-camera.position.set(0, -2, 0.5);
+camera.position.set(0, -3, 0.45);
+
+for (let i = 0; i < circles.length; i++) {
+  circles[i].quaternion.copy(camera.quaternion);
+}
 
 scene.remove(plane);
 
@@ -48,12 +57,15 @@ const clock = new THREE.Clock();
 
 function animate() {
   const t = clock.getElapsedTime();
-  for (let i = 0; i < spheres.length; i++) {
-    const waveX1 = 0.025 * Math.sin(spheres[i].position.x * 6 + t);
-    const waveX2 = 0.0125 * Math.sin(spheres[i].position.x * 8 + t * 2);
-    const waveY1 = 0.025 * Math.sin(spheres[i].position.y * 6 + t * 1.5);
-    const waveY2 = 0.0125 * Math.sin(spheres[i].position.y * 4 + t * 2.5);
-    spheres[i].position.z = waveX1 + waveX2 + waveY1 + waveY2;
+  for (let i = 0; i < circles.length; i++) {
+    //const asd = Math.sin(Math.random() - 1) * 0.0001;
+    const waveX1 = 0.05 * Math.sin(circles[i].position.x * 4 + t * 0.5);
+    const waveX2 = 0.0125 * Math.sin(circles[i].position.x * 8 + t * 2);
+    const waveY1 = 0.035 * Math.sin(circles[i].position.y * 6 + t * 0.5);
+    const waveY2 = 0.0125 * Math.sin(circles[i].position.y * 4 + t * 2.5);
+    circles[i].position.z = waveX1 + waveX2 + waveY1 + waveY2;
+    //circles[i].position.x += asd;
+    //camera.position.x += 0.000001;
   }
 
   requestAnimationFrame(animate);
